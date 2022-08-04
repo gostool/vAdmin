3623d0ff112c0f8bad56475c4c3c43a6eead0c79
tphyhFighting <2363176358@qq.com>
2022-08-04 15:32:00 +0800

    add: LoginCompent

--- src/components/LoginCompent.vue
+++ src/components/LoginCompent.vue
@@ -1,33 +1,59 @@
 <template>
-    <h3>login</h3>
-    {{msg}}
-    <button @click="go"> abc</button>
-    {{count}}
+  <el-form
+    ref="ruleFormRef"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    label-width="120px"
+    class="demo-ruleForm"
+  >
+    <el-form-item label="密码" prop="pass">
+      <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
+    </el-form-item>
+    <el-form-item label="账号" prop="name">
+      <el-input v-model="ruleForm.name" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)"
+        >Submit</el-button
+      >
+      <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
-export default {
-  name: 'LoginCompent',
-  data() {
+import { defineComponent, reactive, ref } from "vue";
+export default defineComponent({
+  setup() {
+    const ruleForm = reactive({
+      pass: '',
+      name: '',
+    });
+    const ruleFormRef = ref();
+    const rules = reactive({
+      pass: [{ required: true, trigger: 'blur' }],
+      name: [{ required: true, trigger: 'blur' }],
+    });
+    const submitForm = (formEl) => {
+      if (!formEl) return
+      formEl.validate((valid) => {
+        if (valid) {
+          console.log('submit!')
+        } else {
+          console.log('error submit!')
+          return false
+        }
+      })
+    };
+
+    const resetForm = (formEl) => {
+      if (!formEl) return
+      formEl.resetFields()
+    };
     return {
-      "count": 0,
+      ruleForm, ruleFormRef, rules, submitForm, resetForm
     }
-  },
-  methods: {
-    go() {
-      this.count++;
-    },
-  },
-  props: {
-    msg: String
   }
-}
+})
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-</style>
-
