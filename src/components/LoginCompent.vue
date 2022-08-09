467c8a7ae1432a61163c620c46fc6e95b4680dfc
tphyhFighting <2363176358@qq.com>
2022-08-09 10:01:46 +0800

    change: background png url

--- src/components/LoginCompent.vue
+++ src/components/LoginCompent.vue
@@ -142,7 +142,7 @@ $dark_gray:#889aa4;
 $light_gray:#eee;
 
 .login-container {
-  background-image: url("~/src/assets/backgound.png");
+  background-image: url("/src/assets/backgound.png");
   background-size: 100% 100%;
   min-height: 100%;
   width: 100%;
